04891e9f8c4096ecee3786341d94319d4186b03b
spikenurlanov <spikenurlanov@gmail.com>
2023-11-13 16:06:17 +0300

    first footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import MainRoutes from "./routes/MainRoutes";
 import Icon from "./components/icon/Icon";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Icon />
       <MainRoutes />
+      <Footer />
     </div>
   );
 };

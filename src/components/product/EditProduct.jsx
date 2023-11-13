fb960def7967d574b9ae3b0a82c85cdd725bb974
Abai <abaiesen04@gmail.com>
2023-11-13 15:20:44 +0600

    add category parameter

--- src/components/product/EditProduct.jsx
+++ src/components/product/EditProduct.jsx
@@ -1,7 +1,7 @@
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, Select, TextField } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useProducts } from '../../contexts/ProductContextProvider';
-import { useParams } from 'react-router';
+import { useParams, useNavigate } from 'react-router';
 
 const EditProduct = () => {
     const {saveChanges, oneProduct, getOneProduct} = useProducts()
@@ -9,9 +9,11 @@ const EditProduct = () => {
     const [product, setProduct] = useState({
         title: '',
         description: '',
+        category: '',
         image: '',
         price: ''
     })
+    const navigate = useNavigate()
     const {id} = useParams()
     useEffect(()=>{
         getOneProduct(id)
@@ -31,12 +33,16 @@ const EditProduct = () => {
     }
     }
     return (
-        <Box sx={{ width: '50vw', height: 500, margin: '20px auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
+        <Box sx={{ width: '50vw', height: 400, margin: '20px auto', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }}>
             <TextField value={product.title} onChange={handleInput} name='title' label="Title" fullWidth variant="outlined" />
             <TextField value={product.description} onChange={handleInput} name='description' label="Description" fullWidth variant="outlined" />
+            <Select/>
             <TextField value={product.image} onChange={handleInput} name='image' label="Image URL" fullWidth variant="outlined" />
             <TextField value={product.price} onChange={handleInput} name='price' label="Price" fullWidth variant="outlined" />
-            <Button onClick={()=>saveChanges(id, product)}>Save changes</Button>
+            <Button onClick={()=>{
+                saveChanges(id, product)
+                navigate('/products')
+                }}>Save changes</Button>
         </Box>
     );
 };
--- src/helpers/consts.js
+++ src/helpers/consts.js
@@ -3,3 +3,4 @@ export const ACTIONS = {
     GET_ONE_PRODUCT: 'GET_ONE_PRODUCT'
 }
 export const API_PRODUCTS = 'http://localhost:8000/products'
+
